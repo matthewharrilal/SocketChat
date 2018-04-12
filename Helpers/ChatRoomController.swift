5b8803f2680740276915e4c998ab203aa7a6551d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-04-12 14:50:01 -0700

    Working standpoint of the setting up the socket connection

--- Helpers/ChatRoomController.swift
+++ Helpers/ChatRoomController.swift
@@ -26,18 +26,19 @@ class ChatRoomController: NSObject {
         // This function is going to be used to connect the client to the server
         
         // Declaring a property that allows us to read data from the stream
-        var readStream: Unmanaged<CFReadStream>?
+        var readStream: Unmanaged<CFReadStream>
         
         // Declaring a propert that allows us to write data to the stream
-        var writeStream: Unmanaged<CFWriteStream>?
+        var writeStream: Unmanaged<CFWriteStream>
         
         // Binding the read and write socket streams to the host running on port 1234 allows us to read and write byte data
-        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, "52.91.109.76", "1234", readStream, writeStream)
+//        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, "52.91.109.76" as CFString!, 1234, readStream, writeStream)
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, "52.91.109.76" as CFString, 1234, readStream, writeStream)
         
         
         // By calling retained value on these unmanaged objects allows ARC to automatically deallocate them therefore there will not be a memory leak
-        self.inputStream = readStream?.takeRetainedValue() // This gives us access to read data from the input stream
-        self.outputStream = writeStream?.takeRetainedValue() // Gives us access to write data to the output stream
+        self.inputStream = readStream.takeRetainedValue() as InputStream // This gives us access to read data from the input stream
+        self.outputStream = writeStream.takeRetainedValue() as OutputStream // Gives us access to write data to the output stream
         
         // Creating a run loop for both of our streams so that they can coordinate and respond to events occuring on the thread, purpose to keep thread busy when there is work to do and sleeping when there is no work
         self.inputStream.schedule(in: .current, forMode: .commonModes) // Not thread safe to call run loops
