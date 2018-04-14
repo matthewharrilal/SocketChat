f3076bb633ca868174670f00645adfac29c6d019
Matthew Harrilal <matthewharrilal@gmail.com>
2018-04-14 14:23:42 -0700

    Implemented the table view abstraction in the view controller for text messages

--- View Controllers/ChatRoomTableViewController.swift
+++ View Controllers/ChatRoomTableViewController.swift
@@ -9,17 +9,33 @@
 import Foundation
 import UIKit
 
-class ChatRoomTableViewController: UITableViewController {
+class ChatRoomViewController: UIViewController, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
     var dataSource = TableViewDataSource(items: [TextMessage]())
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self.dataSource
+        
+        dataSource.configureCell = {(tableView, indexPath) -> UITableViewCell in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ChatRoomTableViewCell
+            let message = self.dataSource.items[indexPath.row]
+            cell.authorOfMessageLabel.text = message.authorOfMessage
+            cell.textOfMessageLabel.text = message.textOfMessage
+            return cell
+        }
+    }
+ 
 }
